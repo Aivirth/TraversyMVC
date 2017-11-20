003bdf4004cdfd00c4d2dc489bc08fe96e693a6b
Tommaso Mannori | Aivirth <aivirth.dev@gmail.com>
2017-11-20 16:23:57 +0100

    Setting up the Core class - grab url get params and load appropriate controller based on them

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -13,8 +13,28 @@ class Core
     protected $currentMethod = 'index';
     protected $params = [];
 
-    public function __construct(){
-        $this->getUrl();
+    public function __construct()
+    {
+        //print_r($this->getUrl());
+        $url = $this->getUrl();
+
+        //Look in controllers for first value
+        if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php'))
+        {
+
+            //if exists, set as controlelr
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 Index
+            unset($url[0]);
+
+        }
+
+        //Require the controller
+        require_once '../app/controllers/' . $this->currentController .  '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
+
     }
 
     /*
@@ -23,7 +43,13 @@ class Core
 
     public  function getUrl()
     {
-        echo $_GET['url'];
+        if(isset($_GET['url']))
+        {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/' , $url);
+            return $url;
+        }
        
     }
 }
