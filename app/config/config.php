c8256e42e15bb18c7767d8e8855a37697680009a
Aivirth <aivirth.work@gmail.com>
2017-11-22 11:57:56 +0100

    config.php file created(base constants) | creation of libraries autoload functions in bootstrap.php

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    //App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT', 'http://localhost/traversy_cms');
+    //Site Name
+    define('SITENAME', 'TraversyMVC');
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,1 +1,2 @@
 <h1><?php echo $data['title']; ?></h1>
+<?php echo APPROOT; ?>
