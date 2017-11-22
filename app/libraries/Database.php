54d12eff9e54d2978ca43aa4c7a6fc41d1b3451b
Aivirth <aivirth.work@gmail.com>
2017-11-22 13:14:11 +0100

    Augmented database class with various functions

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -40,5 +40,64 @@
             }
         }
 
+        //Prepare statement with query
+        public function query($sql)
+        {
+            $this->stmt = $this->dbh->prepare($sql);
+        }
+
+        //Bind values
+        public function bind($param, $value, $type = null)
+        {
+            if(is_null($type))
+            {
+                switch(true)
+                {
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                    break;
+
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                    break;
+
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                    break;
+
+                    default:
+                        $type = PDO::PARAM_STR;
+                    
+                }
+            }
+
+            $this->stmt->bindValue($param, $value, $type);
+        }
+
+        //Execute the prepared statement
+        public function execute()
+        {
+            return $this->stmt->execute();
+        }
+
+        //Get result set as array of objects
+        public function resultSet()
+        {
+            $this->execute();
+            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        //Get single record as object
+        public function single()
+        {
+            $this->execute();
+            return $this->stmt->fetch(PDO::FETCH_OBJ);
+        }
+        
+        //Get row count
+        public function rowCount()
+        {
+            return $this->stmt->rowCount();
+        }
 
     }
