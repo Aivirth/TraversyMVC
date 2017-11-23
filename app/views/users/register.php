c2dd4aeebbc9a04808826207c22758cb9accac1e
Aivirth <aivirth.work@gmail.com>
2017-11-23 13:38:02 +0100

    Register page setup

---
+++ app/views/users/register.php
@@ -0,0 +1,9 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card card-body bg-light mt-5">
+                <h2>Create An Account</h2>
+            </div>
+        </div>
+    </div>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
