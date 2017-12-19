a1ef95bc2a4b39a74f50aa27828a6a502031afb2
Aivirth <aivirth.work@gmail.com>
2017-12-19 17:18:58 +0100

    Added user name on logout button

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -18,6 +18,9 @@
         <ul class="navbar-nav ml-auto">
           <?php if(isset($_SESSION['user_id'])) : ?>
             <li class="nav-item">
+              <a class="nav-link" href="javascript:void(0);">Welcome <?php echo $_SESSION['user_name']; ?></a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
             </li>
           <?php else : ?>
