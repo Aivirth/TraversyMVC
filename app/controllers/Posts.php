4569a3a4bc933dd76d0ee2a0e09ea83090420075
Aivirth <aivirth.work@gmail.com>
2017-12-19 17:05:20 +0100

    Edit Post Functionality

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -86,4 +86,63 @@ class Posts extends Controller {
         $this->view('posts/show', $data);
 
     }
+
+    public function edit($id){
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //Sanitize post array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'title'         => trim($_POST['title']),
+                'body'          => trim($_POST['body']),
+                'id'            => $id,
+                'user_id'       => $_SESSION['user_id'],
+                'title_err'     => '',
+                'body_err'      => ''
+            ];
+
+            //validate title
+            if(empty($data['title'])){
+                $data['title_err'] = 'Please enter title';
+            }
+
+            //validate body
+            if(empty($data['body'])){
+                $data['body_err'] = 'Please enter body';
+            }
+
+            //check if errors are present
+            if(empty($data['title_err']) && empty($data['body_err'])){
+
+                //validated
+                if($this->postModel->updatePost($data)){
+                    flash('post_message', 'Post Updated');
+                    redirect('posts');
+                }else{
+                    die('Something went wrong');
+                }
+               
+            }else{
+                //load view with errors
+                $this->view('posts/edit', $data);
+            }
+
+        }else{
+            //get existing post from model
+            $post = $this->postModel->getPostById($id);
+
+            //check for owner
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            }
+            $data = [
+                'id'    => $id,
+                'title' => $post->title,
+                'body'  => $post->body
+            ];
+            $this->view('posts/edit', $data);
+        }
+        
+    }
 }
--- app/models/Post.php
+++ app/models/Post.php
@@ -39,6 +39,21 @@
             }
         }
 
+        public function updatePost($data){
+            $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id )');
+            // Bind values
+            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':title', $data['title']);
+            $this->db->bind(':body', $data['body']);
+        
+            // Execute
+            if($this->db->execute()){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
         public function getPostById($id){
             $this->db->query('SELECT * FROM posts WHERE id = :id');
             $this->db->bind(':id', $id);
